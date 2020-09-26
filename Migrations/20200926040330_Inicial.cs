69f4f241ebb95c981cedbb435cb1626b41ae5fad
Hudelsis Antigua Mendez <antiguamendez27@gmail.com>
2020-09-27 00:33:51 +0200

    commi inicial

---
+++ Migrations/20200926040330_Inicial.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RegistroPrestamos.Migrations
+{
+    public partial class Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clientes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nombres = table.Column<string>(nullable: true),
+                    Telefono = table.Column<string>(nullable: true),
+                    Cedula = table.Column<string>(nullable: true),
+                    Direccion = table.Column<string>(nullable: true),
+                    Balance = table.Column<float>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clientes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Prestamos",
+                columns: table => new
+                {
+                    PrestamoId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    PersonaId = table.Column<int>(nullable: false),
+                    Cliente = table.Column<string>(nullable: true),
+                    Fecha = table.Column<string>(nullable: true),
+                    Monto = table.Column<float>(nullable: false),
+                    Balance = table.Column<float>(nullable: false),
+                    Concepto = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Prestamos", x => x.PrestamoId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Clientes");
+
+            migrationBuilder.DropTable(
+                name: "Prestamos");
+        }
+    }
+}
